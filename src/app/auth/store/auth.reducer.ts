a43e5aadefd34f199458fae6b427f097d679b16b
Talha <talha@codeteck.com>
2023-03-05 20:54:53 +0500

    working on auth state

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,14 +1,37 @@
-import { User } from "../user.model"
+import { User } from "../user.model";
+import * as AuthActions from "./auth.actions";
 
 export interface State {
-    user : User;
+  user: User;
 }
 
-const initialState : State = {
-    user : null
-}
+const initialState: State = {
+  user: null,
+};
 
-
-export function AuthReducer (state = initialState, action){
-    return state;
+export function AuthReducer(
+  state = initialState,
+  action: AuthActions.AuthActions
+) {
+  switch (action.type) {
+    case AuthActions.LOGIN:
+      const user = new User(
+        action.payload.email,
+        action.payload.id,
+        action.payload.token,
+        action.payload.expirationDate
+      );
+      return {
+        ...state,
+        user: user,
+      };
+    case AuthActions.LOGOUT:
+      return {
+        ...state,
+        user: null,
+      };
+    default:
+      return state;
+  }
+  return state;
 }
